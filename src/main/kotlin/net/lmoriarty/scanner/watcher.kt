655e42ec22ec46241796402fc9547dcb020d8ba5
Samuel Moriarty <insomnia.array@gmail.com>
2017-08-25 18:30:20 +0400

    Fix regional ignore

--- src/main/kotlin/net/lmoriarty/scanner/watcher.kt
+++ src/main/kotlin/net/lmoriarty/scanner/watcher.kt
@@ -46,7 +46,7 @@ class Watcher(val bot: ChatBot) {
      * or null otherwise
      */
     private fun shouldWatch(row: GameRow): GameType? {
-        if (row.currentGame.contains(ignoreRegex)) {
+        if (row.currentGame.toLoweCase().contains(ignoreRegex)) {
             return null
         }
 
